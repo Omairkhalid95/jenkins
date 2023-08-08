ce9c3058cdadd468de0411ba6278376bb0fc3614
Omairkhalid95 <137157096+Omairkhalid95@users.noreply.github.com>
2023-08-08 16:13:36 +0500

    Update docker.groovy

--- vars/docker.groovy
+++ vars/docker.groovy
@@ -2,5 +2,5 @@ def call(Map config = [:]) {
 
 sh "echo ${config.DOCKERHUB_CREDENTIALS_PSW} | sudo docker login -u ${config.DOCKERHUB_CREDENTIALS_USR} --password-stdin"
 echo 'Login Completed'   
-sh "Docker Build successfully with version omair950/${config.tag}"
+echo "Docker Build successfully with version omair950/${config.tag}"
 }
